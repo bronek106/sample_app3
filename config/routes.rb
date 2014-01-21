e739e81385351d2ce48e9dfb70f0a85cdbe2f2d0
Bronek <bronek106@gmail.com>
2014-01-21 13:55:03 +0100

    Rename hello_world to index.html.rb, added route 'hello_world#index'

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SampleApp3::Application.routes.draw do
+  root 'hello_world#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
